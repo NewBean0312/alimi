9c80238d1890fe3a15f565549107b4f192be833a
NewBean0312 <newbean0312@gmail.com>
2023-09-08 19:16:46 +0900

    Tailwind, MUI, MUI 테마 색상 CSS 변수화

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { AppBar, Toolbar } from "@mui/material";
+
 function App() {
   return (
     <>
-      <div><i className="fa-solid fa-house"></i>안녕</div>
+      <AppBar position="static">
+        <Toolbar>
+          <div className="flex-1"></div>
+          <div className="font-bold">HAPPY NOTE</div>
+          <div className="flex-1"></div>
+        </Toolbar>
+      </AppBar>
     </>
   );
 }
