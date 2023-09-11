bdac8f9cb4951d404cc3f10c2e30d3be881d3b42
NewBean0312 <newbean0312@gmail.com>
2023-09-11 15:21:11 +0900

    할 일 form 체크

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,18 +1,49 @@
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 
 export default function WritePage() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    if (form.regDate.value.length == 0) {
+      alert("날짜를 입력해주세요.");
+      form.regDate.focus();
+
+      return;
+    }
+
+    if (form.content.value.length == 0) {
+      alert("내용을 입력해주세요.");
+      form.content.focus();
+
+      return;
+    }
+  };
+
   return (
     <>
-      <div className="flex flex-1 flex-col p-10 gap-7">
-        <TextField type="datetime-local" label="언제 해야 하나요?" focused />
+      <form className="flex flex-1 flex-col p-10 gap-7" onSubmit={onSubmit}>
         <TextField
+          type="datetime-local"
+          name="regDate"
+          label="언제 해야 하나요?"
+          focused
+        />
+        <TextField
+          name="content"
           label="무엇을 해야 하나요?"
           className="flex flex-1"
-          InputProps={{ className: "bg-blue-500 flex-1 flex-col" }}
-          inputProps={{ className: "!bg-pink-500 flex-1 flex-col" }}
+          InputProps={{ className: "flex-1 flex-col" }}
+          inputProps={{ className: "flex-1 flex-col" }}
           multiline
         />
-      </div>
+        <Button type="submit" variant="contained">
+          <span><i className="fa-solid fa-pen"></i></span>
+          <span>&nbsp;&nbsp;</span>
+          <span>할 일 추가</span>
+        </Button>
+      </form>
     </>
   );
 }
